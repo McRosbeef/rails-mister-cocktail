2444a3d31dcf4ad8d912c5c219049bf9c93851cd
Vincent Bres <bres.vincent@gmail.com>
2016-08-11 13:26:01 +0200

    models & controller checked

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,8 +2,10 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :cocktail, presence: true, uniqueness: true
-  validates :ingredient, presence: true, uniqueness: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+
+  validates :ingredient, uniqueness: { scope: :cocktail }
 
   validates :description, presence: true
 end
